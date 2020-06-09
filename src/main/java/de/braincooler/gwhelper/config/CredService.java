496f79d56c786d65c60bbba61771c4ffcd31ce62
braincooler <w,zeller@online.de>
2020-06-09 11:37:45 +0200

    add logging user

--- src/main/java/de/braincooler/gwhelper/config/CredService.java
+++ src/main/java/de/braincooler/gwhelper/config/CredService.java
@@ -1,10 +1,15 @@
 package de.braincooler.gwhelper.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
+
 @Service
 public class CredService {
+    private static final Logger LOGGER = LoggerFactory.getLogger(CredService.class);
 
     @Value("${gw.user}")
     private String gwUser;
@@ -18,5 +23,10 @@ public class CredService {
 
     public String getGwPassword() {
         return gwPassword;
+    }
+
+    @PostConstruct
+    private void init() {
+        LOGGER.info("user '{}'", gwUser);
     }
 }
