88249be51900b7a6e8abdeb3f78689eb2aaf20d6
braincooler <w,zeller@online.de>
2020-07-08 10:10:07 +0200

    copy 2

--- src/main/java/de/braincooler/gwhelper/GwHelperApplication.java
+++ src/main/java/de/braincooler/gwhelper/GwHelperApplication.java
@@ -11,5 +11,6 @@ public class GwHelperApplication {
     public static void main(String[] args) {
         SpringApplication.run(GwHelperApplication.class, args);
         System.out.println("copy");
+        System.out.println("copy2");
     }
 }
